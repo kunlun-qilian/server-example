2d3e6f2f21aeaf7162a9f03d85f2a25fd2492214
pippozq <zengqi0529@163.com>
2022-07-01 15:42:33 +0800

    middleware

--- cmd/server/router/root.go
+++ cmd/server/router/root.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/sirupsen/logrus"
 	"kunlun-qilian/server-example/cmd/server/router/example"
 	"net/http"
 
@@ -11,12 +12,29 @@ func Web(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", gin.H{})
 }
 
+type AuthorizationParam struct {
+	Authorization string `header:"Authorization" binding:"required" `
+}
+
+func Authorization() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		a := AuthorizationParam{}
+		err := ctx.BindHeader(&a)
+		if err != nil {
+			logrus.Warn("Authorization is nil")
+			ctx.JSON(http.StatusBadRequest, err)
+			return
+		}
+
+	}
+}
+
 func NewRooter(r *gin.Engine) {
 
 	r.LoadHTMLGlob("../web/build/index.html")
 	r.StaticFS("/static", http.Dir("../web/build/static"))
 	r.GET("/", Web)
 	// API
-	v1 := r.Group("/api/v1")
+	v1 := r.Group("/api/v1", Authorization())
 	example.CarRouter(v1)
 }
